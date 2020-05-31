283cff0d64a722f5c651f3b06e44e4baa5470b65
Felipe Augusto <augustofelipe147@gmail.com>
2020-05-31 11:54:40 -0300

    Criando um mapa

---
+++ Mapa/Map.py
@@ -0,0 +1,11 @@
+import folium
+m=folium.Map(location=[45.5236, -122.6750])
+folium.CircleMarker(
+    location=[45.5215, -122.6261],
+    radius=50,
+    popup='Laurelhurst Park',
+    color='#3186cc',
+    fill=True,
+    fill_color='#3186cc'
+).add_to(m)
+m.save('index.html')
